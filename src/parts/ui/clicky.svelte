856f33e4c4ee4bb45ea5b17d9b2c8d4c1cfc0330
Sup#2.0 <sup2point0@gmail.com>
2025-05-14 21:12:34 +0100

    feat: implement buttons

---
+++ src/parts/ui/clicky.svelte
@@ -0,0 +1,54 @@
+<!-- @component Clicky
+
+A button which runs a callback when clicked.
+-->
+
+<script lang="ts">
+
+interface Props {
+  text: string;
+  action: () => void;
+}
+
+let { text, action }: Props = $props();
+
+</script>
+
+
+<button onclick={action}>
+  {text}
+</button>
+
+
+<style lang="scss">
+
+@use 'sass:color';
+
+button {
+  padding: 0.5em 1em;
+  background: none;
+  border: 1px solid $col-grey-light;
+  border-radius: 0.5rem;
+  outline-width: 0px;
+  outline-style: solid;
+  outline-color: color.change($col-blue, $alpha: 20%);
+  box-shadow: 0 0.5px 1px $col-grey-light;
+
+  transition: all 0.1s ease-out;
+
+  &:hover {
+    cursor: pointer;
+    color: $col-blue;
+    border-color: $col-blue;
+    outline-width: 2px;
+  }
+
+  &:active {
+    color: $col-purp;
+    border-color: $col-purp;
+    outline-color: color.change($col-purp, $alpha: 20%);
+    transform: scale(97%);
+  }
+}
+
+</style>
